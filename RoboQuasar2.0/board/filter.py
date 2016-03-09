fixed units for the kalman filter

RoboQuasar2.0/board/filter.py
@@ -1,15 +1,15 @@
 """
-    Written by Jason Kagie (modified by Ben Warwick)
+Written by Jason Kagie (modified by Ben Warwick)
 
-    interpreter.py, written for RoboQuasar2.0
-    Version 1/4/2016
-    =========
+interpreter.py, written for RoboQuasar2.0
+Version 1/4/2016
+=========
 
-    Interprets sensor data from board/data.py to x, y, theta (current position).
+Interprets sensor data from board/data.py to x, y, theta (current position).
 
-    Classes:
-        MainFilter - contains a kalman filter tailored for gps, encoder,
-            accelerometer and orientation input
+Classes:
+    MainFilter - contains a kalman filter tailored for gps, encoder,
+        accelerometer and orientation input
 """
 
 import numpy as np
@@ -19,21 +19,14 @@ import math
 
 
 class StateFilter(object):
-    def __init__(self, latitude, longitude, circum, heading,
-                 prev_encoder_value):
+    def __init__(self):
         self.headingKalman = HeadingKalman()
-        self.placeKalman = PositionKalman((latitude, longitude), circum,
-                                          heading, prev_encoder_value)
-        self.time = time.time()
-        self.dt = 0
+        self.placeKalman = PositionKalman()
 
-    def update(self, gps_lat, gps_lon, encoder_counts, accel_x, accel_y, gyro_z,
-               heading):
-        self.dt = 0.105
-        self.time = time.time()
-        phi = self.headingKalman.update(heading, gyro_z, self.dt)
-        x, y = self.placeKalman.update(gps_lat, gps_lon, encoder_counts,
-                                       accel_x, accel_y, phi, self.dt)
+    def update(self, gps_x, gps_y, change_dist, accel_x, accel_y, gyro_z, heading, dt):
+        phi = self.headingKalman.update(heading, gyro_z, dt)
+        x, y = self.placeKalman.update(gps_x, gps_y, change_dist,
+                                       accel_x, accel_y, phi, dt)
         return x, y, phi
 
 
@@ -59,47 +52,25 @@ class HeadingKalman(object):
 
 
 class PositionKalman(object):
-    def __init__(self, origin, wheel_radius, start_heading, prev_encoder_data):
-        self.origin = (
-            origin[0] / 3600,
-            origin[1] / 3600
-        )
-
-        self.origin_rad = (
-            self.origin[0] * math.pi / 180,
-            self.origin[1] * math.pi / 180
-        )
-
-        self.wheel_radius = 2 * math.pi * wheel_radius
+    def __init__(self):
         self.filter = pykalman.KalmanFilter()
         self.filt_state_mean = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         self.covariance = np.identity(6)
-        self.displacement_angle = start_heading
-        self.earth_radius = 6371000
-        self.prev_encoder = prev_encoder_data
-        self.seconds_conversion = 1850.0 / 60.0  # 1 sec = this many meters
 
-    def update(self, gps_lat, gps_lon, encoder_value, accel_x, accel_y, heading,
-               dt):
-        x_gps, y_gps = self.geo_dist(gps_lat, gps_lon)
-
-        encoder_counts = encoder_value - self.prev_encoder
-        self.prev_encoder = encoder_value
-
-        observation = np.array([x_gps, y_gps,
-                                encoder_counts, accel_x, accel_y])
+    def update(self, gps_x, gps_y, change_dist, accel_x, accel_y, heading, dt):
+        observation = np.array([gps_x, gps_y, change_dist, accel_x, accel_y])
 
         if (math.cos(heading) == 0):
-            y_coeff = self.wheel_radius * dt / math.sin(heading)
+            y_coeff = dt / math.sin(heading)
             x_coeff = 0
 
         elif (math.sin(heading) == 0):
-            x_coeff = self.wheel_radius * dt / math.cos(heading)
+            x_coeff = dt / math.cos(heading)
             y_coeff = 0
 
         else:
-            x_coeff = 0.5 * self.wheel_radius * dt / math.cos(heading)
-            y_coeff = 0.5 * self.wheel_radius * dt / math.sin(heading)
+            x_coeff = 0.5 * dt / math.cos(heading)
+            y_coeff = 0.5 * dt / math.sin(heading)
 
         observation_matrix = np.array(
             [[1, 0, 0, 0, 0, 0],
@@ -125,20 +96,3 @@ class PositionKalman(object):
             observation_matrix=observation_matrix)
 
         return self.filt_state_mean[0], self.filt_state_mean[1]  # x, y
-
-    def geo_dist(self, latitude, longitude):
-        """assuming the latitude and lontitude are given in seconds"""
-        phi1 = self.origin_rad[0]
-        phi2 = latitude / 3600 * math.pi / 180
-        dphi = phi2 - self.origin_rad[0]
-        dlambda = longitude / 3600 * math.pi / 180 - self.origin_rad[1]
-
-        a = (math.sin(dphi / 2) ** 2 +
-             math.cos(phi1) * math.cos(phi2) *
-             math.sin(dlambda / 2) ** 2)
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-        dist = self.earth_radius * c
-        angle = math.atan2(latitude - self.origin[0],
-                           longitude - self.origin[1])
-        return dist * math.cos(angle), dist * math.sin(angle)

RoboQuasar2.0/map/map_parser.py
@@ -6,126 +6,135 @@ import sys
 import os
 import numpy as np
 import copy
-from scipy.spatial.distance import cdist as distance
 
 sys.path.insert(0, '../')
 
 import config
 
+class Map():
+    def __init__(self, map_name, shift_angle, origin_lat, origin_long, directory=None):
+        self.data = self.get_map(map_name, directory)
+        self.origin_lat  = origin_lat * np.pi / 180
+        self.origin_long = origin_long * np.pi / 180
+        self.shift_matrix = np.array(
+            [[np.cos(shift_angle), np.sin(shift_angle)],
+             [-np.sin(shift_angle), np.cos(shift_angle)]]
+        )
+        self.deg_to_m = 111226.343
 
-def get_map(directory=None):
-    maps_dir = config.get_dir(":maps")
-    if directory is None:
-        for file_name in os.listdir(maps_dir):
-            if len(file_name) >= 5 and file_name[-4:] == '.csv':
-                directory = maps_dir + file_name
-                break
+        self.convert_data()
+        self.data = np.dot(self.data, self.shift_matrix)
+
+    def convert_data(self):
+        """
+        goes through every lat, long pair and replaces it with the x y distance
+        from the origin point
+
+        :return: None
+        """
+        for i in range(len(self.data)):
+            point_lat  = self.data[i][0] * np.pi / 180
+            point_long = self.data[i][1] * np.pi / 180
+            lat_mean = (point_lat + self.origin_lat) / 2
+            x = (point_long - self.origin_long) * np.cos(lat_mean)
+            y = (point_lat - self.origin_lat)
+            self.data[i][0] = x * self.deg_to_m
+            self.data[i][1] = y * self.deg_to_m
+
+
+    @staticmethod
+    def get_map(map_name, directory=None):
         if directory is None:
-            raise FileNotFoundError("No valid maps to choose from.")
+            directory = config.get_dir(":maps")
 
-    if not os.path.isdir(directory):
-        directory = maps_dir + directory
+        with open(directory + map_name, 'r') as csvfile:
+            map_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            parsed = [[float(row[0]), float(row[1])] for row in map_reader]
+            return np.array(parsed)
 
-    with open(directory, 'r') as csvfile:
-        map_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        parsed = [[float(row[0]), float(row[1])] for row in map_reader]
-        return parsed
+    @staticmethod
+    def write_map(data, directory=None, file_name=None):
+        if directory is None:
+            directory = config.get_dir(":maps")
+        if file_name is None:
+            file_name = time.strftime("%c").replace(":", ";")
+        with open(directory + file_name + ".csv", 'w') as csv_file:
+            map_writer = csv.writer(csv_file, delimiter=',',
+                                    quotechar='|',
+                                    quoting=csv.QUOTE_MINIMAL)
+            for row in data:
+                assert len(row) == 2
+                map_writer.writerow(row)
 
+    @staticmethod
+    def remove_duplicates(map, write_output=True, directory=None, map_name=None):
+        map = np.array(map)
 
-def write_map(data, directory=None, file_name=None):
-    if directory is None:
-        directory = config.get_dir(":maps")
-    if file_name is None:
-        file_name = time.strftime("%c").replace(":", ";")
-    with open(directory + file_name + ".csv", 'w') as csv_file:
-        map_writer = csv.writer(csv_file, delimiter=',',
-                                quotechar='|',
-                                quoting=csv.QUOTE_MINIMAL)
-        for row in data:
-            assert len(row) == 2
-            map_writer.writerow(row)
+        duplicates = np.where(np.diff(map, axis=0) == 0)
 
+        map = np.delete(map, duplicates[0], 0)
+        if write_output:
+            write_map(map, directory, map_name)
+        return map
 
-def remove_duplicates(map, write_output=True, directory=None, map_name=None):
-    map = np.array(map)
+    @staticmethod
+    def convert_gpx(file_name, directory=None):
+        if directory is None:
+            directory = config.get_dir(":gpx")
 
-    duplicates = np.where(np.diff(map, axis=0) == 0)
+        with open(directory + file_name, 'r') as gpx_file:
+            contents = gpx_file.read()
+            data = []
 
-    map = np.delete(map, duplicates[0], 0)
-    if write_output:
-        write_map(map, directory, map_name)
-    return map
+            while len(contents) > 2:
+                lat_index_start = contents.find("lat") + 5
+                lat_index_end = contents.find('"', lat_index_start)
+                latitude = contents[lat_index_start: lat_index_end]
 
-def separate_min_sec(value):
-    minutes = int(value)
-    seconds = (value - minutes) * 60
-    return minutes, seconds
+                contents = contents[lat_index_end:]
 
+                lon_index_start = contents.find("lon") + 5
+                lon_index_end = contents.find('"', lon_index_start)
+                longitude = contents[lon_index_start: lon_index_end]
 
-def merge_min_sec(minutes, seconds):
-    return minutes + seconds / 60
+                data = [latitude, longitude]
 
-def convert_gpx(file_name, directory=None):
-    if directory is None:
-        directory = config.get_dir(":gpx")
+                contents = contents[lon_index_end:]
 
-    with open(directory + file_name, 'r') as gpx_file:
-        contents = gpx_file.read()
-        data = []
+            file_name = file_name[:-4] + " converted"
+            write_map(data, directory, file_name)
 
-        while len(contents) > 2:
-            lat_index_start = contents.find("lat") + 5
-            lat_index_end = contents.find('"', lat_index_start)
-            latitude = contents[lat_index_start: lat_index_end]
-
-            contents = contents[lat_index_end:]
-
-            lon_index_start = contents.find("lon") + 5
-            lon_index_end = contents.find('"', lon_index_start)
-            longitude = contents[lon_index_start: lon_index_end]
-
-            try:
-                data.append([separate_min_sec(float(latitude))[1],
-                             separate_min_sec(float(longitude))[1]])
-            except:
-                pass
-
-            contents = contents[lon_index_end:]
-
-        file_name = file_name[:-4] + " converted"
-        write_map(data, directory, file_name)
-
-class MapParser:
-    def __init__(self, map_name):
-        self.map = get_map(map_name)
+class Binder:
+    def __init__(self, map_name, shift_angle, origin_lat, origin_long, directory=None):
+        self.map = Map(map_name, shift_angle, origin_lat, origin_long, directory)
         # set to None so that it will be able to start at any point on the track
         self.prev_bind = None
 
     def bind(self, position):
-        if (self.prev_bind is None or self.prev_bind >= (len(self.map) - 1)
+        if (self.prev_bind is None or self.prev_bind >= (len(self.map.data) - 1)
                 or self.prev_bind < 0):
             # finds the smallest distance between the point and the map
             self.prev_bind = self.find_nearest(position)
-            return self.map[self.prev_bind + 1]
+            return self.map.data[self.prev_bind + 1]
 
-        for index in range(self.prev_bind, len(self.map)):
+        for index in range(self.prev_bind, len(self.map.data)):
             if self.is_near(index, position):
                 self.prev_bind = index
-                return self.map[index + 1]
+                return self.map.data[index + 1]
 
         for index in range(self.prev_bind):
             if self.is_near(index, position):
                 self.prev_bind = index
-                return self.map[index + 1]
+                return self.map.data[index + 1]
 
         self.prev_bind = self.find_nearest(position)
-        return self.map[self.prev_bind + 1]
+        return self.map.data[self.prev_bind + 1]
 
     def find_nearest(self, position):
-        map_dist = [0] * len(self.map)
+        map_dist = [0] * len(self.map.data)
         for index in range(len(map_dist)):
-            dlat = abs(float(self.map[index][0] - position[0]))
-            dlong = abs(float(self.map[index][1] - position[1]))
+            dlat = abs(float(self.map.data[index][0] - position[0]))
+            dlong = abs(float(self.map.data[index][1] - position[1]))
             dist = ((dlat ** 2) + (dlong ** 2)) ** 0.5
             map_dist[index] = dist
         smallest_value = min(map_dist)
@@ -133,24 +142,29 @@ class MapParser:
         return index
 
     def is_near(self, index, position):
-        dx = abs(float(self.map[index][0]) - position[0])
-        dy = abs(float(self.map[index][1]) - position[1])
+        dx = abs(float(self.map.data[index][0]) - position[0])
+        dy = abs(float(self.map.data[index][1]) - position[1])
         dist = ((dx ** 2) + (dy ** 2)) ** 0.5
 
-        if index + 2 < len(self.map):
-            acc_dlat = abs(float(self.map[index][0] - self.map[index + 1][0]))
-            acc_dlong = abs(float(self.map[index][1] - self.map[index + 1][1]))
+        if index + 2 < len(self.map.data):
+            acc_dlat = abs(float(self.map.data[index][0] - self.map.data[index + 1][0]))
+            acc_dlong = abs(float(self.map.data[index][1] - self.map.data[index + 1][1]))
 
         else:
-            acc_dlat = abs(float(self.map[index][0] - self.map[index - 1][0]))
-            acc_dlong = abs(float(self.map[index][1] - self.map[index - 1][1]))
+            acc_dlat = abs(float(self.map.data[index][0] - self.map.data[index - 1][0]))
+            acc_dlong = abs(float(self.map.data[index][1] - self.map.data[index - 1][1]))
 
         accuracy = ((acc_dlat ** 2 + acc_dlong ** 2) ** 0.5) / 2
 
         return dist <= accuracy
 
 
-def test():
+def binder_test():
+    binder = Binder("Mon Mar  7 17;54;59 2016 GPS Map.csv", 0,
+        26.611000061, 56.40650177)
+    for row in binder.map.data:
+        print("%f,%f" % (row[0], row[1]))
+
     pos1 = (26.6, 56.329)
     pos2 = (26.594, 56.34)
     pos3 = (26.59, 56.35)
@@ -159,43 +173,35 @@ def test():
 
     pos = binder.bind(pos1)
     pre_bind = binder.prev_bind
-    print((binder.map[pre_bind]), "map[pre_bind]")
+    print((binder.map.data[pre_bind]), "map[pre_bind]")
     print((pos, pre_bind), "pos, pre_bind")
 
     pos = binder.bind(pos2)
     pre_bind = binder.prev_bind
-    print((binder.map[pre_bind]), "map[pre_bind]")
+    print((binder.map.data[pre_bind]), "map[pre_bind]")
     print((pos, pre_bind), "pos, pre_bind")
 
     pos = binder.bind(pos3)
     pre_bind = binder.prev_bind
-    print((binder.map[pre_bind]), "map[pre_bind]")
+    print((binder.map.data[pre_bind]), "map[pre_bind]")
     print((pos, pre_bind), "pos, pre_bind")
 
     pos = binder.bind(pos4)
     pre_bind = binder.prev_bind
-    print((binder.map[pre_bind]), "map[pre_bind]")
+    print((binder.map.data[pre_bind]), "map[pre_bind]")
     print((pos, pre_bind), "pos, pre_bind")
 
     pos = binder.bind(pos5)
     pre_bind = binder.prev_bind
-    print((binder.map[pre_bind]), "map[pre_bind]")
+    print((binder.map.data[pre_bind]), "map[pre_bind]")
     print((pos, pre_bind), "pos, pre_bind")
 
+def test_shift():
+    test_obj = Map("2015-11-08 07_58_30.csv", 0,
+                   40.4405106744, -79.9425712322)
+    for row in test_obj.data:
+        print("%f,%f" % (row[0], row[1]))
 
 if __name__ == '__main__':
-    binder = MapParser("Mon Mar  7 17;54;59 2016 GPS Map.csv")
-
-    # pos = binder.bind((26.6156005859, 56.423500061))
-    # pre_bind = binder.prev_bind
-    # print((binder.map[pre_bind]), "map[pre_bind]")
-    # print((pos, pre_bind), "pos, pre_bind")
-    #
-    # # binder = Binder("Mon Mar  7 17;54;59 2016 GPS Map.csv")
-    #
-    # pos = binder.bind((26.605298996, 56.478302002))
-    # pre_bind = binder.prev_bind
-    # print((binder.map[pre_bind]), "map[pre_bind]")
-    # print((pos, pre_bind), "pos, pre_bind")
-
-    test()
+    # test_shift()
+    binder_test()