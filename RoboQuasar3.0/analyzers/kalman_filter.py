Set up and edited the kalman filter. Still gives horrible results, maybe due to incorrect units of gps

RoboQuasar3.0/analyzers/kalman_filter.py
@@ -20,52 +20,13 @@ import math
 
 class StateFilter(object):
     def __init__(self):
-        self.headingKalman = HeadingKalman()
         self.placeKalman = PositionKalman()
 
-    def update(self, gps_x, gps_y, change_dist, accel_x, accel_y, gyro_z, heading, dt):
-        phi = self.headingKalman.update(heading, gyro_z, dt)
+    def update(self, gps_x, gps_y, change_dist, accel_x, accel_y, heading, dt):
+        #changed so that it no longer runs a filter on the yaw value
         x, y = self.placeKalman.update(gps_x, gps_y, change_dist,
-                                       accel_x, accel_y, phi, dt)
-        return x, y, phi
-
-
-class HeadingKalman(object):
-    def __init__(self):
-        self.filter = pykalman.KalmanFilter()
-        self.filt_state_mean = np.array([0.0, 0.0])  # phi0 = 0, Vang0 = 0
-        self.covariance = np.array([[1.0,0.0],[0.0,1.0]])
-        self.obs_matrix = np.identity(2)
-        #observation matrices are how you weight the inputs
-        self.observation_covariance = np.array([[1.0,0.0],[0.0,15.0]])
-
-    def update(self, heading, gyro_z, dt):
-        trans_matrix = np.array([[1, dt],
-                                 [0, 1]])
-        obs = np.array([heading, gyro_z])
-        self.filt_state_mean, self.covariance = self.filter.filter_update(
-            filtered_state_mean=self.filt_state_mean,
-            filtered_state_covariance=self.covariance,
-            observation=obs,
-            transition_matrix=trans_matrix,
-            observation_matrix=self.obs_matrix,
-            observation_covariance = self.observation_covariance)
-
-
-        #if the inputs are out of its bounds,
-        #reinitialize it to that bound so that it doesnt mess up
-        if self.filt_state_mean[0] < 0:
-            self.filt_state_mean[0] = 0
-            self.filter = pykalman.KalmanFilter(
-                    initial_state_mean = self.filt_state_mean,
-                    initial_state_covariance = self.covariance)
-        if self.filt_state_mean[0] >360:
-            self.filt_state_mean[0] = 360
-            self.filter = pykalman.KalmanFilter(
-                    initial_state_mean = self.filt_state_mean,
-                    initial_state_covariance = self.covariance)
-
-        return self.filt_state_mean[0]
+                                       accel_x, accel_y, heading, dt)
+        return x, y, heading
 
 
 class PositionKalman(object):
@@ -76,9 +37,9 @@ class PositionKalman(object):
     def __init__(self):
         self.obs_covariance = np.array([[1.0, 0.0, 0.0, 0.0, 0.0],
                                         [0.0, 1.0, 0.0, 0.0, 0.0],
-                                        [0.0, 0.0, 1.0, 0.0, 0.0],
-                                        [0.0, 0.0, 0.0, 1.0, 0.0],
-                                        [0.0, 0.0, 0.0, 0.0, 1.0]])
+                                        [0.0, 0.0, 100.0, 0.0, 0.0],
+                                        [0.0, 0.0, 0.0, 100.0, 0.0],
+                                        [0.0, 0.0, 0.0, 0.0, 100.0]])
         self.filter = pykalman.KalmanFilter(
                 observation_covariance = self.obs_covariance)
         self.filt_state_mean = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])