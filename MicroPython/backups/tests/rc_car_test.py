8183a9ec78461ed81fa38e30822be9efa1114b80
Woz4tetra <woz4tetra@gmail.com>
2016-06-08 01:36:55 +0000

    servos work with micropython + raspberry pi

--- MicroPython/backups/tests/rc_car_test.py
+++ MicroPython/backups/tests/rc_car_test.py
@@ -1,26 +1,26 @@
 from pyb import Servo
 
-print("tests:"
-print("- servo")
-print("- motor")
+print("tests:")
+print("- (s)ervo")
+print("- (m)otor")
 test_name = input("test to run:")
-if test_name == "servo"
-    servo = Servo(1)
+if test_name == "s":
+    servo = Servo(3)
 
     while True:
         value = input("> ")
         if value == 'r':
-            servo.angle(-13)
+            servo.angle(-12)
         elif value == 'l':
             servo.angle(14)
         elif value == 'f':
-            servo.angle(0)
+            servo.angle(2)
 
         try:
             servo.angle(int(value))
         except ValueError:
             pass
-elif test_name == "motor":
+elif test_name == "m":
     pass
 else:
     print("invalid test name")
