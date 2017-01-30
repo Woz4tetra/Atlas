3ca3e0dc490a97ef6ba3039fb40c4a42fdd90959
Tabatha Pina <Tabatha Pina>
2017-01-30 12:42:01 -0500

    Added gps code for arduino and python
    Python code is complete, but needs to be checked to prevent raw gps string
    interfearance
    Arduino code needs to be checked for serial type, as well as the proper
    passing of gps raw sentences

---
+++ Robots/roboquasar0.1/Arduino/GPS_Arduino/GPS_Arduino.ino
@@ -0,0 +1,169 @@
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+Adafruit_GPS GPS(&Serial1);
+
+#define DEFAULT_RATE 115200
+#define WHOIAM "GPS"
+#define LED13 13
+#define GPSECHO true
+
+boolean usingInterrpt = false;
+
+Adafruit_GPS GPS(&Serial1);
+HardwareSerial mySerial = Serial1;
+bool led_state = false;
+bool paused = true;
+
+void writeWhoiam()
+{
+    Serial.print("iam");
+    Serial.print(WHOIAM);
+    Serial.print('\n');
+}
+
+void writeInit()
+{
+    Serial.print("init:");
+    //maybe?
+    Serial.print('\n');
+}
+
+void setLed(bool state)
+{
+    led_state = state;
+    digitalWrite(LED13, led_state);
+}
+
+void pause()
+{
+    Serial.print("stopping\n");
+    paused = true;
+}
+
+void unpause() {
+    paused = false;
+}
+
+void readSerial()
+{
+    while (Serial.available())
+    {
+        String command = Serial.readStringUntil('\n');
+
+        if (command.equals("whoareyou")) {
+            writeWhoiam();
+        }
+        else if (command.equals("init?")) {
+            writeInit();
+        }
+        else if (command.equals("start"))
+        {
+            setLed(HIGH);
+            unpause();
+        }
+        else if (command.equals("stop"))
+        {
+            setLed(LOW);
+            pause();
+        }
+    }
+}
+
+void updateGPS()
+{
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  if (GPS.newNMEAreceived()) 
+  {
+    if (!GPS.parse(GPS.lastNMEA()))
+    {
+      return;  
+    } 
+    
+    if (GPS.fix) {
+      Serial.print(GPS.hour, DEC); Serial.print('\t');
+      Serial.print(GPS.minute, DEC); Serial.print('\t');
+      Serial.print(GPS.seconds, DEC); Serial.print('\t');
+      Serial.print(GPS.milliseconds); Serial.print('\t');
+      
+      Serial.print(GPS.day, DEC); Serial.print('\t');
+      Serial.print(GPS.month, DEC); Serial.print('\t');
+      Serial.print(GPS.year, DEC); Serial.print('\t'); 
+      Serial.print((int)GPS.fixquality); Serial.print('\t')
+      
+      Serial.print(GPS.latitude, 4); Serial.print('\t')
+      Serial.print(GPS.lat); Serial.print('\t')
+      Serial.print(GPS.longitude, 4); Serial.print('\t')
+      Serial.print(GPS.lon); Serial.print('\t')
+      Serial.print(GPS.latitudeDegrees, 4); Serial.print('\t')
+      Serial.print(GPS.longitudeDegrees, 4); Serial.print('\t')
+  
+      Serial.print(GPS.speed); Serial.print('\t')
+      Serial.print(GPS.angle); Serial.print('\t')
+      Serial.print(GPS.altitude); Serial.print('\t')
+      Serial.print((int)GPS.satellites); Serial.print('\t')
+  
+      Serial.print('\n')
+  }
+}
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) Serial.print(c);  
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+void setup() 
+{
+  Serial.begin(DEFAULT_RATE);
+  GPS.begin(9600);
+
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
+
+  useInterrupt(true);
+
+  /* Initialise the sensor */
+  if(!GPS.LOCUS_StartLogger())
+  {
+    Serial.print("No GPS detected");
+  }
+
+  useInterrupt(true)
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if (!paused) 
+  {
+      updateGPS();
+  }
+  else 
+  {
+      delay(100);
+  }
+  readSerial();
+}
--- Robots/roboquasar0.1/sensors/gps.py
+++ Robots/roboquasar0.1/sensors/gps.py
@@ -3,10 +3,62 @@ from atlasbuggy.robot.robotobject import RobotObject
 
 class GPS(RobotObject):
     def __init__(self, enabled=True):
+        self.hour = 0
+        self.minute = 0
+        self.second = 0
+        self.milliseconds = 0
+
+        self.day = 0
+        self.month = 0
+        self.year = 0
+
+        self.fix_quality = 0
+
+        self.latitude = 0
+        self.lat = 0
+        self.longitude = 0
+        self.lon = 0
+        self.latitude_degree = 0
+        self.longitude_degree = 0
+
+        self.knots = 0
+        self.angle = 0
+        self.altitude = 0
+        self.satellites = 0
+
         super(GPS, self).__init__("gps", enabled)
 
     def receive_first(self, packet):
+        # this shouldn't be called
         pass
 
     def receive(self, timestamp, packet):
-        pass
+        data = packet.split("\t")
+
+        # time
+        self.hour = float(data[0])
+        self.minute = float(data[1])
+        self.second = float(data[2])
+        self.milliseconds = float(data[3])
+
+        #date
+        self.day = float(data[4])
+        self.month = float (data[5])
+        self.year = float(data[6])
+
+        # fix info
+        self.fix_quality = float(data[7])
+
+        # location info
+        self.latitude = float(data[8])
+        self.lat = float(data[9])
+        self.longitude = float(data[10])
+        self.lon = float(data[11])
+        self.latitude_degree = float(data[12])
+        self.longitude_degree = float(data[13])
+
+        # cool shit
+        self.knots = float(data[14])
+        self.angle = float(data[15])
+        self.altitude= float(data[16])
+        self.satellites = float(data[17])
