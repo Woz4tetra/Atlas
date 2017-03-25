3a04435f3234846443261af21b7f99e0ca288c6a
osguus <ovg@andrew.cmu.edu>
2017-03-25 14:58:10 -0400

    added pipeline2, roboquasar can switch between pipelines in init

--- Robots/roboquasar0.1/algorithms/pipeline2.py
+++ Robots/roboquasar0.1/algorithms/pipeline2.py
@@ -1,12 +1,14 @@
-import bisect
 from queue import Queue
 from threading import Event, Thread
 
 import cv2
 import numpy as np
 
+class Pipeline2:
 
-class Pipeline:
+    threshold = 70
+    calibration_frame = 3058
+
     def __init__(self, camera, separate_read_thread=True):
         self.camera = camera
         self.status = None
@@ -16,6 +18,17 @@ class Pipeline:
         self.paused = False
         self.pause_updated = False
         self.frame = None
+
+        # Filter for validating line
+        self.width = None
+        self.filter = np.random.rand(5,5,3) # creates a 5x5 filter for each R,G,B map
+        self.on_screen = None # tells whether the desired line is on the screen
+        self.moving_up = None # gives amount the percentage is changing, aim to get this to 0 for smooth change
+        self.frame_counter = 0 # counts frames to help in filter creation
+        # frame 3058 shows the desired line
+
+        # Other
+        self.prev_mid = None
 
         self.safety_threshold = 0.1
         self.safety_value = 0.0
@@ -78,9 +91,13 @@ class Pipeline:
 
                         while not self.frame_queue.empty():
                             frame = self.frame_queue.get()
+
                             if frame is None:
                                 self.status = "exit"
                                 break
+
+                            if self.width is None:
+                                self.width = frame.shape[1]
 
                             self.frame = self.pipeline(frame)
                     else:
@@ -116,89 +133,28 @@ class Pipeline:
         else:
             return False
 
+    def filter_init(self, frame):
+        # allocate a 5x5 area to be a filter
+        filter_frame = frame[(mid[0]-2):(mid[0]+2)][(mid[1]-2):(mid[1]+2)]
+        
+
+
     def pipeline(self, frame):
+        self.frame_counter += 1  # counts frames
+
         self.prev_safe_value = self.safety_value
         frame, lines, self.safety_value = self.hough_detector(frame.copy(), True)
+
+        if self.frame_counter == 3058:
+            self.filter_init(frame)
+
+        # allows us to find the frame we want
+        print(self.frame_counter)
 
         frame[10:40, 20:90] = self.safety_colors[int(self.safety_value * 10)]
         cv2.putText(frame, "%0.1f%%" % (self.safety_value * 100), (30, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
 
-        # frame = self.kernel_threshold(self.camera.frame)
-        # contours, perimeters = self.get_contours(frame, 0.025, 2)
-        # if len(contours) > 0:
-        #     frame = self.draw_contours(self.camera.frame, contours)
-        #
-        #     frame, results = self.get_pavement_edge(perimeters, contours, frame)
-        #
-        # frame = self.average_color(frame)
-
         return frame
-
-    def get_pavement_edge(self, perimeters, contours, frame):
-        results = []
-        if perimeters[-1] > 2000:
-            contour = contours[-1]
-            for index in range(1, len(contour)):
-                point1 = contour[index - 1][0]
-                point2 = contour[index][0]
-                diff = point2 - point1
-                angle = np.arctan2(diff[0], diff[1])
-                if -np.pi / 2 - 0.5 < angle < -np.pi / 2 + 0.5:
-                    frame = cv2.line(frame, tuple(point1.tolist()), tuple(point2.tolist()), (50, 200, 0), thickness=10)
-                    results.append((angle, point1, point2))
-        return frame, results
-
-    def average_color(self, frame):
-        avg_color = np.uint8(np.average(np.average(frame, axis=0), axis=0))
-        height, width = self.camera.frame.shape[0:2]
-        avg_color_top = self.average_color(frame[:height // 2])
-        avg_color_bot = self.average_color(frame[height // 2:])
-
-        frame[0:height // 8, 0:width // 4, :] = avg_color_top
-        frame[height // 8:height // 4, 0:width // 4, :] = avg_color_bot
-        frame = cv2.putText(frame, str(avg_color_top)[1:-1], (0, height // 8 - 5), cv2.FONT_HERSHEY_PLAIN,
-                            1, (255, 255, 255))
-        frame = cv2.putText(frame, str(avg_color_bot)[1:-1], (0, height // 4 - 5), cv2.FONT_HERSHEY_PLAIN,
-                            1, (255, 255, 255))
-        return avg_color, frame
-
-    def kernel_threshold(self, frame):
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.equalizeHist(frame)
-
-        value, frame = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-        # noise removal
-        kernel = np.ones((3, 3), np.uint8)
-        opening = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=2)
-
-        # sure background area
-        sure_bg = cv2.dilate(opening, kernel, iterations=3)
-
-        # Finding sure foreground area
-        dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-        ret, sure_fg = cv2.threshold(dist_transform, 0.1 * dist_transform.max(), 255, 0)
-
-        return dist_transform
-
-        # Finding unknown region
-        # sure_fg = np.uint8(sure_fg)
-        # unknown = cv2.subtract(sure_bg, sure_fg)
-
-        # Marker labelling
-        # ret, markers = cv2.connectedComponents(sure_fg)
-        #
-        # # Add one to all labels so that sure background is not 0, but 1
-        # markers = markers + 1
-        #
-        # # Now, mark the region of unknown with zero
-        # markers[unknown == 255] = 0
-        #
-        # markers = cv2.watershed(self.camera.frame, markers)
-        # # self.camera.frame[markers == -1] = [255, 0, 0]
-        #
-        # blank = np.ones(self.camera.frame.shape[0:2], dtype=np.uint8) * 255
-        # blank[markers == -1] = 0
 
     def hough_detector(self, input_frame, day_mode):
         # blur = cv2.medianBlur(input_frame, 5)
@@ -218,55 +174,13 @@ class Pipeline:
         safety_percentage = self.draw_lines(input_frame, lines)
 
         output_frame = cv2.add(cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR), input_frame)
-        if day_mode:
-            output_frame = np.concatenate((output_frame, blur), axis=1)
-        else:
-            output_frame = np.concatenate((output_frame, cv2.cvtColor(blur, cv2.COLOR_GRAY2BGR)), axis=1)
+
+        # if day_mode:
+        #     output_frame = np.concatenate((output_frame, blur), axis=1)
+        # else:
+        #     output_frame = np.concatenate((output_frame, cv2.cvtColor(blur, cv2.COLOR_GRAY2BGR)), axis=1)
+
         return output_frame, lines, safety_percentage
-
-    def sobel_filter(self, frame):
-        # frame = cv2.Laplacian(frame, cv2.CV_64F, ksize=3)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.medianBlur(frame, 3)
-        frame = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=3)
-
-        frame = np.absolute(frame)
-        frame = np.uint8(frame)
-        return frame
-
-    def threshold(self, input_frame):
-        frame = cv2.cvtColor(input_frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.medianBlur(frame, 11)
-        frame = cv2.equalizeHist(frame)
-
-        thresh_val, frame = cv2.threshold(frame, 0, 255, cv2.THRESH_OTSU)
-        # thresh_val, frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
-        return frame
-
-    def draw_contours(self, frame, contours):
-        return cv2.drawContours(frame.copy(), contours, -1, (0, 0, 255), 3)
-
-    def get_contours(self, binary, epsilon=None, num_contours=None):
-        binary, contours, hierarchy = cv2.findContours(binary.copy(), cv2.RETR_TREE,
-                                                       cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        sig_contours = []
-        perimeters = []
-
-        for contour in contours:
-            perimeter = cv2.arcLength(contour, closed=False)
-            index = bisect.bisect(perimeters, perimeter)
-            if epsilon is not None:
-                approx = cv2.approxPolyDP(contour, epsilon * perimeter, False)
-                sig_contours.insert(index, approx)
-            else:
-                sig_contours.insert(index, contour)
-            perimeters.insert(index, perimeter)
-
-        if num_contours is None:
-            return sig_contours, perimeters
-        else:
-            return sig_contours[-num_contours:], perimeters[-num_contours:]
 
     def draw_lines(self, frame, lines, draw_threshold=30):
         height, width = frame.shape[0:2]
@@ -290,23 +204,58 @@ class Pipeline:
 
                 y3 = int(y0 - width / 2 * a)
 
-                # if y2 > largest_y:
-                #     largest_y = y2
-                #     largest_coords = (x1, y1), (x2, y2)
                 if y3 > largest_y:
                     largest_y = y3
                     largest_coords = (x1, y1), (x2, y2)
 
                 cv2.line(frame, (x1, y1), (x2, y2), (150, 150, 150), 2)
-                # cv2.circle(frame, (x0, y0), 10, (150, 255, 50), 2)
+
                 counter += 1
                 if counter > draw_threshold:
                     break
 
             if largest_coords is not None:
                 cv2.line(frame, largest_coords[0], largest_coords[1], (0, 0, 255), 2)
+
             return largest_y / height
+
         return 0.0
+
+    def run_filter(self, frame, filter):
+        mid = self.prev_mid
+
+
+    def find_borderpoints(self, frame, coords):
+        height, width = frame.shape[0:2]
+        p1 = coords[0]
+        p2 = coords[1]
+
+        # find the slope between the points
+        slope = (p2[1] - p1[1]) / (p2[0] - p1[0])
+
+        # find the left-most point
+        left_point = (0,
+                      int(p1[1] + slope * (-p1[0])))
+        # find the right-most point
+        right_point = (width,
+                       int(p1[1] + slope * (width - p1[0])))
+
+        # if self.prev_coords == None:
+        #     self.prev_coords = [left_point, right_point]
+        # elif self.is_valid_point(frame, coords, Pipeline2.threshold):
+        #     self.prev_coords = [left_point, right_point]
+        self.prev_mid = ((left_point[0]+right_point[0])/2, (left_point[1]+right_point[1])/2)
+
+        print([left_point, right_point])
+
+        cv2.circle(frame, left_point, 3, (255, 255, 255), 2)
+        cv2.circle(frame, right_point, 3, (255, 255, 255), 2)
+
+    # def is_valid_point(self, frame, coords, threshold):
+    #     val = self.run_filter(frame, self.filter)
+    #     if val < threshold:
+    #         return True
+    #     return False
 
     def close(self):
         self.exit_event.set()
